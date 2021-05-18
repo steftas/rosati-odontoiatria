a6efd8477fb257908510deb2c7aa0383946b20be
Stefan <stefantasic@palmhr.net>
2021-05-18 14:59:25 +0200

    areapazienti updates

--- src/views/Areapazienti.vue
+++ src/views/Areapazienti.vue
@@ -6,122 +6,161 @@
           <div class="login-div">
             <h3>Area pazienti</h3>
             <button @click="logout">Logout</button>
+
+            <p class="text-left mt-5">
+              Gentile Paziente,
+              <br />
+              In questa area riservata le proponiamo una serie di approfondimenti riguardo la salute orale.
+              <br />
+              Troverà documenti da noi redatti o scrupolosamente controllati al fine di garantirle un'informazione
+              scientificamente corretta in un ambito -la salute- in cui spesso inesattezze e approssimazioni generano
+              confusione.
+            </p>
           </div>
 
           <div class="row download-row">
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/4xSNQ5JU61pBLYHe?ref=Link" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>La diga di gomma</span>
-              </a>
-            </div>
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/88dStYrwHSJ0NqQZ?ref=Link" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>Protesi fissa cementata su denti naturali</span>
-              </a>
-            </div>
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/nzF2e3GSXZ26KtOq?ref=Link" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>Ricostruzione dentale tramite perno in fibra</span>
-              </a>
-            </div>
+            <div class="col-12">
+              <ul>
+                <li>
+                  <a href="https://sway.office.com/4xSNQ5JU61pBLYHe?ref=Link" target="_blank" rel="noopener">
+                    <span>La diga di gomma</span>
+                  </a>
+                </li>
 
-
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/gacdjKeqPDjvZMlu?ref=Link&loc=mysways" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>Igiene dentale professionale</span>
-              </a>
-            </div>            
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/K9RBzbg0EfKqWyUJ?ref=Link&loc=mysways" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>Ricostruzione dentale diretta (otturazione)</span>
-              </a>
+                <li>
+                  <a href="https://sway.office.com/88dStYrwHSJ0NqQZ?ref=Link" target="_blank" rel="noopener">
+                    <span>Protesi fissa cementata su denti naturali</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://sway.office.com/nzF2e3GSXZ26KtOq?ref=Link" target="_blank" rel="noopener">
+                    <span>Ricostruzione dentale tramite perno in fibra</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://sway.office.com/gacdjKeqPDjvZMlu?ref=Link&loc=mysways"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <span>Igiene dentale professionale</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://sway.office.com/K9RBzbg0EfKqWyUJ?ref=Link&loc=mysways"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <span>Ricostruzione dentale diretta (otturazione)</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://sway.office.com/1lmQ9WDLBsyVyHTG?ref=Link&loc=mysways"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <span>Levigatura radicolare</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://sway.office.com/Nmty6pkj4t7iG30E?ref=Link&loc=mysways"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <span>Intarsio in ceramica/composito</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://sway.office.com/DIOO6gUV5m5otjKU?ref=Link&loc=mysways"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <span>Protesi dentale su impianti</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://sway.office.com/xHWgrJRdhfdXnVBT?ref=Link&loc=mysways"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <span>Aumento di volume osseo</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://sway.office.com/f2FRTBjCGNSgVfYC?ref=Link&loc=mysways"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <span>Trattamento canalare</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://sway.office.com/gOsMkh3WvN1ml718?ref=Link&loc=mysways"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <span>La patologia parodontale</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://sway.office.com/JfslArm9BpM06Ydy?ref=Link&loc=mysways"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <span>La radiografia odontoiatrica</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://sway.office.com/t91ASzZu8qWO8yUR?ref=Link&loc=mysways"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <span>Fluoroprofilassi</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://sway.office.com/FPjHXhuvjXK0hqD2?ref=Link&loc=mysways"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <span>Lesioni non cariose dei denti</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://sway.office.com/7olc386FrOTg6xof?ref=Link&loc=mysways"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <span>Otturazioni, quale materiale?</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://sway.office.com/b2jjB7zN40fN1yyo?ref=Link" target="_blank" rel="noopener">
+                    <span>Lesioni dello smalto nel piccolo paziente</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://sway.office.com/mApQ5Y5rhnKW18r3?ref=Link&loc=mysways"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <span>Sbiancamento dentale</span>
+                  </a>
+                </li>
+              </ul>
             </div>
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/1lmQ9WDLBsyVyHTG?ref=Link&loc=mysways" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>Levigatura radicolare</span>
-              </a>
-            </div>
-
-
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/Nmty6pkj4t7iG30E?ref=Link&loc=mysways" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>Intarsio in ceramica/composito</span>
-              </a>
-            </div>
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/DIOO6gUV5m5otjKU?ref=Link&loc=mysways" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>Protesi dentale su impianti</span>
-              </a>
-            </div>
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/xHWgrJRdhfdXnVBT?ref=Link&loc=mysways" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>Aumento di volume osseo</span>
-              </a>
-            </div>
-
-
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/f2FRTBjCGNSgVfYC?ref=Link&loc=mysways" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>Trattamento canalare</span>
-              </a>
-            </div>
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/gOsMkh3WvN1ml718?ref=Link&loc=mysways" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>La patologia parodontale</span>
-              </a>
-            </div>
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/JfslArm9BpM06Ydy?ref=Link&loc=mysways" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>La radiografia odontoiatrica</span>
-              </a>
-            </div>
-
-
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/t91ASzZu8qWO8yUR?ref=Link&loc=mysways" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>Fluoroprofilassi</span>
-              </a>
-            </div>
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/FPjHXhuvjXK0hqD2?ref=Link&loc=mysways" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>Lesioni non cariose dei denti</span>
-              </a>
-            </div>
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/7olc386FrOTg6xof?ref=Link&loc=mysways" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>Otturazioni, quale materiale?</span>
-              </a>
-            </div>
-
-
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/b2jjB7zN40fN1yyo?ref=Link" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>Lesioni dello smalto nel piccolo paziente</span>
-              </a>
-            </div>
-            <div class="col-md-4 col-sm-6">
-              <a href="https://sway.office.com/mApQ5Y5rhnKW18r3?ref=Link&loc=mysways" target="_blank" rel="noopener">
-                <img src="@/assets/images/sway_logo.png" />
-                <span>Sbiancamento dentale</span>
-              </a>
-            </div>
-            
           </div>
         </div>
       </div>
@@ -130,78 +169,69 @@
 </template>
 
 <script lang="ts">
-import firebase from 'firebase';
+import firebase from "firebase";
 
 export default {
-  name: 'area-pazineti',
+  name: "area-pazineti",
   mounted() {
     const screenHeight = window.innerHeight;
-    const appHeight = document.getElementById('app').clientHeight;
+    const appHeight = document.getElementById("app").clientHeight;
 
     if (appHeight < screenHeight) {
-      console.log(document.getElementById('area-pazienti'));
-      const a = document.getElementById('area-pazienti');
-      a.style.minHeight = (a.clientHeight + (screenHeight - appHeight)) + 'px';
+      console.log(document.getElementById("area-pazienti"));
+      const a = document.getElementById("area-pazienti");
+      a.style.minHeight = a.clientHeight + (screenHeight - appHeight) + "px";
     }
   },
   methods: {
     logout() {
-      firebase.auth().signOut().then(() => {
-        this.$router.replace('login');
-      });
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.replace("login");
+        });
     },
   },
 };
 </script>
 
 <style lang="scss">
-  #area-pazienti {
-    padding: 30px 0;
-    min-height: 350px;
+#area-pazienti {
+  padding: 30px 0;
+  min-height: 350px;
 
-    .login-div {
-      padding: 25px;
-      background-color: #FFFFFF;
-      text-align: center;
+  .login-div {
+    padding: 25px;
+    background-color: #ffffff;
+    text-align: center;
 
-      h3 {
-        font-size: 30px;
-        font-weight: 300;
-        line-height: 1.16;
-        margin: 0 0 15px;
-      }
-
-      button {
-        background-color: #314057;
-        padding: 0.375rem 2.75rem;
-        border-radius: 0;
-        border: 0;
-        color: #FFFFFF;
-      }
+    h3 {
+      font-size: 30px;
+      font-weight: 300;
+      line-height: 1.16;
+      margin: 0 0 15px;
     }
 
-    .download-row {
-      a {
-        display: flex;
-        align-content: center;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        color: #2c3c54;
-        padding: 30px 0;
+    button {
+      background-color: #314057;
+      padding: 0.375rem 2.75rem;
+      border-radius: 0;
+      border: 0;
+      color: #ffffff;
+    }
+  }
 
-        &:hover {
-          text-decoration: none;
-        }
+  .download-row {
+    ul {
+      li {
+        padding: 8px 16px;
 
-        img {
-          width: 100%;
-          max-width: 50px;
-          max-height: 50px;
-          display: block;
-          margin: 0 0 15px;
+        a {
+          color: #2c3c54;
         }
       }
     }
   }
+}
 </style>
